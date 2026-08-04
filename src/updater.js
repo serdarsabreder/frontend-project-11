fb5591de6460521a6e1d92dc22c30bdc37b9d042
serdar <serdarsabreder17@gmail.com>
2026-08-04 23:10:51 +0300

    Use recursive setTimeout for feed updates to avoid overlapping requests

--- src/updater.js
+++ src/updater.js
@@ -3,30 +3,33 @@ import { Toast } from 'bootstrap';
 import fetchFeed from './rss.js';
 import { uniqueId } from './utils.js';
 
-const initUpdater = (state, elements) => {
+const updateFeeds = (state, elements) => {
   const { toast, toastBody } = elements;
   const toastInstance = Toast.getOrCreateInstance(toast);
 
-  const updateFeeds = () => {
-    const requests = state.feeds.map((feed) => fetchFeed(feed.url)
-      .then((data) => {
-        const existingLinks = state.posts
-          .filter((post) => post.feedId === feed.id)
-          .map((post) => post.link);
-        const newPosts = data.items
-          .filter((item) => !existingLinks.includes(item.link))
-          .map((item) => ({ ...item, id: uniqueId(), feedId: feed.id }));
-        if (newPosts.length > 0) {
-          state.posts.push(...newPosts);
-          toastBody.textContent = i18next.t('newPosts', { title: feed.title });
-          toastInstance.show();
-        }
-      })
-      .catch(() => null));
-    Promise.all(requests).catch(() => null);
-  };
+  const requests = state.feeds.map((feed) => fetchFeed(feed.url)
+    .then((data) => {
+      const existingLinks = state.posts
+        .filter((post) => post.feedId === feed.id)
+        .map((post) => post.link);
+      const newPosts = data.items
+        .filter((item) => !existingLinks.includes(item.link))
+        .map((item) => ({ ...item, id: uniqueId(), feedId: feed.id }));
+      if (newPosts.length > 0) {
+        state.posts.push(...newPosts);
+        toastBody.textContent = i18next.t('newPosts', { title: feed.title });
+        toastInstance.show();
+      }
+    })
+    .catch(() => null));
 
-  setInterval(updateFeeds, 5000);
+  return Promise.all(requests)
+    .catch(() => null)
+    .finally(() => setTimeout(() => updateFeeds(state, elements), 5000));
+};
+
+const initUpdater = (state, elements) => {
+  setTimeout(() => updateFeeds(state, elements), 5000);
 };
 
 export default initUpdater;
